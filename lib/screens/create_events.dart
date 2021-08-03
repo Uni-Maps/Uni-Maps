d7a0219443ffb5abcae7d623cabdeeb68509682f
3ricLu <EricLu.Canada@gmail.com>
2021-08-03 19:42:32 -0400

    More updates and improvements

--- lib/screens/create_events.dart
+++ lib/screens/create_events.dart
@@ -38,57 +38,100 @@ class _Create_EventsState extends State<Create_Events> {
           child: Column(children: <Widget>[
         // Top App Bar Container
         Container(
-          // Add padding
-          decoration: new BoxDecoration(
-            color: Color(0xff493657),
-          ),
-          child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Padding(
-                  // Change this to children, add another container
-                  padding: EdgeInsets.fromLTRB(15, 15, 15, 15),
-                  child: Container(
-                    child: IconButton(
-                      icon: Icon(Icons.arrow_back),
-                      onPressed: () {
-                        print("line 58");
-                      },
-                      iconSize: 32,
-                      color: Colors.white,
+            // Add padding
+            decoration: new BoxDecoration(
+              color: Color(0xff493657),
+            ),
+            child: Row(children: <Widget>[
+              Align(
+                  alignment: Alignment.centerLeft,
+                  child: Padding(
+                    // Change this to children, add another container
+                    padding: EdgeInsets.fromLTRB(15, 30, 15, 30),
+                    child: Container(
+                      child: IconButton(
+                        icon: Icon(Icons.arrow_back),
+                        onPressed: () {
+                          print("line 58");
+                        },
+                        iconSize: 32,
+                        color: Colors.white,
+                      ),
                     ),
-                  ),
-                ),
-                RichText(
-                  text: TextSpan(children: [
-                    TextSpan(
-                        text: "Create Events",
-                        style: TextStyle(
-                            fontFamily: "Lato Bold",
-                            color: Colors.white,
-                            fontSize: 25))
-                  ]),
-                )
-              ]),
-        ),
+                  )),
+              Align(
+                  alignment: Alignment.center,
+                  child: RichText(
+                    text: TextSpan(children: [
+                      TextSpan(
+                          text: "Create Events",
+                          style: TextStyle(
+                              fontFamily: "Lato Bold",
+                              color: Colors.white,
+                              fontSize: 25))
+                    ]),
+                  )),
+            ])),
 
         // Container that has all the input boxes
         Container(
-          padding: EdgeInsets.fromLTRB(15, 20, 15, 0),
-          // Add padding and change colours
-          decoration: new BoxDecoration(
-              color: Color(0xff493657),
-              borderRadius: new BorderRadius.only(
-                  topLeft: const Radius.circular(20.0),
-                  bottomLeft: const Radius.circular(20.0),
-                  bottomRight: const Radius.circular(20.0),
-                  topRight: const Radius.circular(20.0))),
-          child: Padding(
-            padding: EdgeInsets.fromLTRB(15, 45, 15, 0),
-            child: Container(),
-          ),
-        )
-        // Uni-Maps Title
+            padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+            child: Container(
+              padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+              // Add padding and change colours
+              decoration: new BoxDecoration(
+                  color: Color(0xff493657),
+                  borderRadius: new BorderRadius.only(
+                      topLeft: const Radius.circular(20.0),
+                      bottomLeft: const Radius.circular(20.0),
+                      bottomRight: const Radius.circular(20.0),
+                      topRight: const Radius.circular(20.0))),
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(15, 15, 15, 0),
+                child: Column(
+                  children: <Widget>[
+                    // Event Name Input Box
+                    Padding(
+                        padding: EdgeInsets.fromLTRB(20, 0, 20, 15),
+                        child: TextFormField(
+                          style: TextStyle(color: Colors.white),
+                          decoration: InputDecoration(
+                              enabledBorder: new OutlineInputBorder(
+                                borderSide: new BorderSide(color: Colors.white),
+                                borderRadius: BorderRadius.circular(14),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                                borderRadius: BorderRadius.circular(14),
+                              ),
+                              labelText: 'Event Name',
+                              labelStyle: TextStyle(color: Colors.white)),
+                        )),
+
+                    // Event description
+                    Padding(
+                        padding: EdgeInsets.fromLTRB(20, 0, 20, 15),
+                        child: TextFormField(
+                          style: TextStyle(color: Colors.white),
+                          decoration: InputDecoration(
+                              enabledBorder: new OutlineInputBorder(
+                                borderSide: new BorderSide(color: Colors.white),
+                                borderRadius: BorderRadius.circular(14),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                                borderRadius: BorderRadius.circular(14),
+                              ),
+                              labelText: 'Event Description',
+                              labelStyle: TextStyle(color: Colors.white)),
+                        ))
+                    // Date
+                    // Location Drop Down
+                    // Category Drop Down
+                  ],
+                ),
+              ),
+            ))
       ]))
     ]))));
   }
