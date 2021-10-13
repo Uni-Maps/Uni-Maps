803f3f07373106becb3c909b470f256ba762ec1d
3ricLu <EricLu.Canada@gmail.com>
2021-10-12 20:23:42 -0400

    update !

--- lib/screens/categories_page.dart
+++ lib/screens/categories_page.dart
@@ -6,6 +6,7 @@ import 'package:winhacks/services/auth.dart';
 import 'package:winhacks/shared/constants.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:winhacks/screens/styles.dart';
+import 'package:winhacks/screens/models.dart';
 
 class Categories_Page extends StatefulWidget {
   final Function toggleView;
@@ -129,7 +130,7 @@ class _CategoriesPage_State extends State<Categories_Page> {
                   child: Padding(
                 padding: const EdgeInsets.fromLTRB(20, 15, 0, 0),
                 child: Text(
-                  'Suggested for you',
+                  'Browse by Category',
                   style: TextStyle(
                       fontFamily: "Roboto Bold",
                       color: Colors.white,
@@ -137,202 +138,22 @@ class _CategoriesPage_State extends State<Categories_Page> {
                 ),
               )),
 
-              // Slider
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
-                child: CarouselSlider(
-                  options: CarouselOptions(
-                      height: 230.0,
-                      autoPlay: true,
-                      autoPlayInterval: Duration(seconds: 3)),
-                  items: [1, 2, 3, 4, 5].map((i) {
-                    return Builder(
-                      builder: (BuildContext context) {
-                        return Container(
-                          width: MediaQuery.of(context).size.width,
-                          margin: EdgeInsets.symmetric(horizontal: 10.0),
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.only(
-                                  topRight: Radius.circular(10.0),
-                                  bottomRight: Radius.circular(10.0),
-                                  topLeft: Radius.circular(10.0),
-                                  bottomLeft: Radius.circular(10.0)),
-                              // Slider colour
-                              color: Color(0xff493657)),
-                          child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: <Widget>[
-                                Container(
-                                  width: MediaQuery.of(context).size.width,
-                                  child: Image(
-                                    // Temporary image for the slider
-                                    // Be careful since the image needs to be an exact size right now
-                                    image:
-                                        AssetImage('assets/images/pancake.png'),
-                                    fit: BoxFit.cover,
-                                  ),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Text(
-                                    'Event Title $i',
-                                    style: TextStyle(
-                                        fontFamily: "Lato Bold",
-                                        color: Colors.white,
-                                        fontSize: 16),
-                                  ),
-                                ),
-                                Padding(
-                                  padding:
-                                      const EdgeInsets.fromLTRB(8, 0, 0, 4),
-                                  child: Row(
-                                    children: [
-                                      Icon(
-                                        Icons.calendar_today,
-                                        color: Colors.white,
-                                      ),
-                                      Padding(
-                                        padding: const EdgeInsets.fromLTRB(
-                                            4, 0, 0, 0),
-                                        child: Text(
-                                          'Date $i',
-                                          style: TextStyle(
-                                              fontFamily: "Lato Bold",
-                                              color: Colors.white,
-                                              fontSize: 14),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                                Padding(
-                                  padding:
-                                      const EdgeInsets.fromLTRB(8, 0, 0, 4),
-                                  child: Row(
-                                    children: [
-                                      Icon(
-                                        Icons.access_time_rounded,
-                                        color: Colors.white,
-                                      ),
-                                      Padding(
-                                        padding: const EdgeInsets.fromLTRB(
-                                            4, 0, 0, 0),
-                                        child: Text(
-                                          'Time $i',
-                                          style: TextStyle(
-                                              fontFamily: "Lato Bold",
-                                              color: Colors.white,
-                                              fontSize: 14),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                )
-                              ]),
-                        );
-                      },
-                    );
-                  }).toList(),
-                ),
-              ),
-
-              // Sub Title 2
-              Container(
-                  child: Padding(
-                padding: const EdgeInsets.fromLTRB(20, 15, 0, 0),
-                child: Text(
-                  'Featured',
-                  style: TextStyle(
-                      fontFamily: "Roboto Bold",
-                      color: Colors.white,
-                      fontSize: 20),
-                ),
-              )),
-
-              // Featured
-              Padding(
-                padding: const EdgeInsets.fromLTRB(10, 20, 0, 0),
-                child: Container(
-                  width: (MediaQuery.of(context).size.width) * 0.7,
-                  margin: EdgeInsets.symmetric(horizontal: 10.0),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                          topRight: Radius.circular(10.0),
-                          bottomRight: Radius.circular(10.0),
-                          topLeft: Radius.circular(10.0),
-                          bottomLeft: Radius.circular(10.0)),
-                      // Slider colour
-                      color: Color(0xff493657)),
-                  child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        // Gesture Detection goes here
-
-                        Container(
-                          width: MediaQuery.of(context).size.width,
-                          child: Image(
-                            // Temporary image for the slider
-                            // Be careful since the image needs to be an exact size right now
-                            image: AssetImage('assets/images/pancake.png'),
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text(
-                            'Event Title x',
-                            style: TextStyle(
-                                fontFamily: "Lato Bold",
-                                color: Colors.white,
-                                fontSize: 16),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.fromLTRB(8, 0, 0, 4),
-                          child: Row(
-                            children: [
-                              Icon(
-                                Icons.calendar_today,
-                                color: Colors.white,
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.fromLTRB(4, 0, 0, 0),
-                                child: Text(
-                                  'Date x',
-                                  style: TextStyle(
-                                      fontFamily: "Lato Bold",
-                                      color: Colors.white,
-                                      fontSize: 14),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.fromLTRB(8, 0, 0, 4),
-                          child: Row(
-                            children: [
-                              Icon(
-                                Icons.access_time_rounded,
-                                color: Colors.white,
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.fromLTRB(4, 0, 0, 0),
-                                child: Text(
-                                  'Time x',
-                                  style: TextStyle(
-                                      fontFamily: "Lato Bold",
-                                      color: Colors.white,
-                                      fontSize: 14),
-                                ),
-                              ),
-                            ],
-                          ),
-                        )
-                      ]),
-                ),
+              // Categories
+              Column(
+                children: [
+                  Row(
+                    children: [categories(), categories()],
+                  ),
+                  Row(
+                    children: [categories(), categories()],
+                  ),
+                  Row(
+                    children: [categories(), categories()],
+                  ),
+                  Row(
+                    children: [categories(), categories()],
+                  )
+                ],
               ),
             ],
           ),
