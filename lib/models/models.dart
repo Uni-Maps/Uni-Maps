3bc085b6ca05dd5e0048cdff6b4ab3a57dac78d7
3ricLu <EricLu.Canada@gmail.com>
2021-11-27 17:38:01 -0500

    Finished Organization Details
    - Finished Organization Details Page
    - Added in new images
    - Currently the calendar button gets to this page

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -384,3 +384,84 @@ savedEvents() {
     ),
   ));
 }
+
+events() {
+  return (Padding(
+    padding: const EdgeInsets.fromLTRB(10, 20, 10, 0),
+    child: Container(
+      width: 350,
+      height: 100,
+      margin: EdgeInsets.symmetric(horizontal: 10.0),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+              topRight: Radius.circular(10.0),
+              bottomRight: Radius.circular(10.0),
+              topLeft: Radius.circular(10.0),
+              bottomLeft: Radius.circular(10.0)),
+          // Slider colour
+          color: Color(0xff493657)),
+      child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            // Gesture Detection goes here
+
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Container(
+                width: 70,
+                height: 70,
+                child: Image(
+                  // Temporary image for the slider
+                  // Be careful since the image needs to be an exact size right now
+                  image: AssetImage('assets/images/pancake.png'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+
+            // Second Container for all the information and button in column format
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        "Health and Wellness",
+                        style: TextStyle(
+                            fontFamily: "Lato Bold",
+                            color: Colors.white,
+                            fontSize: 14),
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 8, 0, 0),
+                    child: Container(
+                      width: 200,
+                      height: 40,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          Flexible(
+                            child: Text(
+                                "Creating a community for students to live a healthy lifestyle",
+                                style: TextStyle(
+                                    fontFamily: "Lato Regular",
+                                    color: Colors.white,
+                                    fontSize: 12)),
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ]),
+    ),
+  ));
+}
