be632f73f2de17bf039eeb68874d8988d4751719
3ricLu <EricLu.Canada@gmail.com>
2022-01-13 09:13:53 -0500

    background change

--- lib/screens/eventDetails.dart
+++ lib/screens/eventDetails.dart
@@ -24,7 +24,7 @@ class _EventDetailsPageState extends State<EventDetailsPage> {
           decoration: BoxDecoration(
               image: DecorationImage(
                   // Background Image
-                  image: AssetImage("assets/images/blank_background.png"),
+                  image: AssetImage("assets/images/background.png"),
                   fit: BoxFit.fill)),
         ),
       ),
@@ -33,6 +33,46 @@ class _EventDetailsPageState extends State<EventDetailsPage> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
             topAppBar(title, screenWidth),
+            Container(
+                padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+                // Outer container that is purple
+                child: Container(
+                  width: screenWidth - 20,
+                  height: screenHeight / 2,
+                  padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                  // Add padding and change colours
+                  decoration: new BoxDecoration(
+                      color: Color(0xff493657),
+                      borderRadius: new BorderRadius.only(
+                          topLeft: const Radius.circular(20.0),
+                          bottomLeft: const Radius.circular(20.0),
+                          bottomRight: const Radius.circular(20.0),
+                          topRight: const Radius.circular(20.0))),
+                  child: Padding(
+                    padding: EdgeInsets.fromLTRB(15, 25, 15, 0),
+                    child: Column(
+                      children: <Widget>[
+                        RichText(text: TextSpan(text: "Hello world 2")),
+                        Padding(
+                            padding: EdgeInsets.fromLTRB(20, 0, 20, 20),
+                            // Event Description
+                            child: Container(
+                              height: screenHeight / 3,
+                              width: screenWidth - 40,
+                              child:
+                                  RichText(text: TextSpan(text: "Hello World")),
+                              decoration: BoxDecoration(
+                                  borderRadius: new BorderRadius.only(
+                                      topLeft: const Radius.circular(10.0),
+                                      bottomLeft: const Radius.circular(10.0),
+                                      bottomRight: const Radius.circular(10.0),
+                                      topRight: const Radius.circular(10.0)),
+                                  border: Border.all(color: Colors.white)),
+                            )),
+                      ],
+                    ),
+                  ),
+                ))
           ]))
     ]));
   }
